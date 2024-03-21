e83ba04222edbc78e66b3da7dbef7eb2f5ffe121
deependra2843 <djgharia@gmail.com>
2024-03-21 11:50:35 +0530

    [added jwt, cors config]

---
+++ Harmony-heaven-server-master/src/main/java/com/dailycodework/hotelmaven/security/user/HotelUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.dailycodework.hotelmaven.security.user;
+
+import com.dailycodework.hotelmaven.model.User;
+import com.dailycodework.hotelmaven.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class HotelUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return HotelUserDetails.buildUserDetails(user);
+    }
+}
